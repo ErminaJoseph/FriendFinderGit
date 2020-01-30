435595e4996a5560ba786287f17067d7428058d8
Ermina_Joseph <ermina.joseph@gmail.com>
2020-01-30 14:11:27 -0500

    adding file to repo

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friendsArray = [
+    {
+        name: "Elijah",
+        photo: "https://i.dailymail.co.uk/i/pix/2016/05/19/22/34651EE600000578-0-image-a-104_1463694099724.jpg",
+        scores: [
+            5,
+            5,
+            3,
+            2,
+            1,
+            5,
+            4,
+            3,
+            3,
+            2
+        ]
+    },
+    {
+        name: "AJ",
+        photo: "https://scontent.fbos1-1.fna.fbcdn.net/v/t31.0-8/p960x960/21993148_1147918628676778_14533851487101468_o.jpg?_nc_cat=101&_nc_ohc=mc02mOR1vqAAX_1GIlq&_nc_ht=scontent.fbos1-1.fna&_nc_tp=6&oh=a12d3f00fa7ee93ed8f49e33758bcb2f&oe=5E95AD83",
+        scores: [
+            4,
+            4,
+            3,
+            3,
+            5,
+            1,
+            2,
+            5,
+            3,
+            1
+        ]
+    },
+    {
+        name: "Barack",
+        photo: "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzg5ODI4MTEw/barack-obama-12782369-1-402.jpg",
+        scores: [
+            1,
+            5,
+            3,
+            5,
+            3,
+            4,
+            1,
+            2,
+            3,
+            5,
+        ]
+    },
+    {
+        name: "Sam",
+        photo: "https://survivingdating.com/wp-content/uploads/2010/07/man21-e1279690467560.jpg",
+        scores: [
+            4,
+            2,
+            1,
+            3,
+            5,
+            5,
+            2,
+            2,
+            1,
+            5
+        ]
+    },
+    {
+        name: "Darnell",
+        photo: "https://nextshark.com/wp-content/uploads/2017/08/Tinder-Niket-Biswas.jpg",
+        scores: [
+            3,
+            1, 
+            4,
+            5,
+            3,
+            3,
+            2,
+            2,
+            1,
+            4
+        ]
+    },
+    {
+        name: "Naim",
+        photo: "https://i.pinimg.com/originals/b3/70/68/b3706867ccb65e35719c7c2ab3fb5c21.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            2,
+            3,
+            1,
+            5,
+            2,
+            4,
+            3
+        ]
+    },
+    {
+        name: "KC",
+        photo: "https://image.shutterstock.com/image-photo/profile-shot-young-man-wearing-260nw-185377658.jpg",
+        scores: [
+            5,
+            2, 
+            3,
+            1,
+            5,
+            3,
+            2,
+            3,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Valentin",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLG9MBVOBhCv9T32_40Jdoe1e_ByVfT3O_-JPZEwrkb3n9LYADg&s",
+        scores: [
+            4,
+            3,
+            1,
+            2,
+            5,
+            5,
+            4,
+            2,
+            2,
+            3
+        ]
+    },
+    {
+        name: "Ashton",
+        photo: "https://cdn.sugardaddyforme.com/photos/38/ZRI4454538_1_tmb.JPG",
+        scores: [
+            2,
+            3,
+            2,
+            4,
+            2,
+            1,
+            1,
+            5,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Greg",
+        photo: "https://img.huffingtonpost.com/asset/5b0f14991a0000c704ce023d.jpeg?ops=crop_0_216_1350_1286%2Cscalefit_720_noupscale",
+        scores: [
+            5,
+            4,
+            3,
+            5,
+            1,
+            2,
+            4,
+            4,
+            5,
+            1
+        ]
+    }
+
+]
+
+module.exports = friendsArray;
